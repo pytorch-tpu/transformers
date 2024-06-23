7103f7a54f4f0d793290e2d1e9e235114bbe6e4b
ZhiyuLi-goog <lizhiyu@google.com>
2024-06-23 01:56:14 -0700

    update

--- dpo_new.py
+++ dpo_new.py
@@ -41,6 +41,28 @@ from transformers import logging
 from typing import Dict, Union, List, Tuple, Literal
 import torch
 
+from datetime import datetime
+import os
+import getpass
+
+def get_local_dir(prefixes_to_resolve: List[str]) -> str:
+    """Return the path to the cache directory for this user."""
+    for prefix in prefixes_to_resolve:
+        if os.path.exists(prefix):
+            return f"{prefix}/{getpass.getuser()}"
+    os.makedirs(prefix)
+    return f"{prefix}/{getpass.getuser()}"
+    
+
+def get_local_run_dir(exp_name: str, local_dirs: List[str]) -> str:
+    """Create a local directory to store outputs for this run, and return its path."""
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d_%H-%M-%S_%f")
+    run_dir = f"{get_local_dir(local_dirs)}/{exp_name}_{timestamp}"
+    os.makedirs(run_dir, exist_ok=True)
+    return run_dir
+
+OmegaConf.register_new_resolver("get_local_run_dir", lambda exp_name, local_dirs: get_local_run_dir(exp_name, local_dirs))
 logger = logging.get_logger(__name__)
 
 
