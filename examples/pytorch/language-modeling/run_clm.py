2cb554764e290c474aa16f06c3f660b74b16c4f1
Yifei Teng <yifeit@google.com>
2024-10-24 15:13:03 -0700

    Get rid of resize_token_embeddings

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -470,12 +470,6 @@ def main():
         n_params = sum({p.data_ptr(): p.numel() for p in model.parameters()}.values())
         logger.info(f"Training new model from scratch - Total size={n_params/2**20:.2f}M params")
 
-    # We resize the embeddings only when necessary to avoid index errors. If you are creating a model from scratch
-    # on a small vocab and want a smaller embedding size, remove this test.
-    embedding_size = model.get_input_embeddings().weight.shape[0]
-    if len(tokenizer) > embedding_size:
-        model.resize_token_embeddings(len(tokenizer))
-
     # Apply 2D sharding.
     # We apply sharding annotations before running the tokenizer, since
     # `training_args.main_process_first` may involve a `mark_step` which will materialize the
