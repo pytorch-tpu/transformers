78fc68199cb50f3b605b2b8038b9f954cd38b09d
Bhavya Bahl <bbahl@google.com>
2025-03-03 23:45:30 +0000

    Fix error for fsdp_axis

--- examples/pytorch/language-modeling/run_clm.py
+++ examples/pytorch/language-modeling/run_clm.py
@@ -722,10 +722,10 @@ def main():
         num_devices = xr.global_runtime_device_count()
         expert_axis = config.expert_parallel_axis
         # Ignore tensor axis for now. It doesn't do anything.
-        ici_mesh_shape = (1, fsdp_axis, expert_axis, 1)
         dcn_axis = NUM_TPU_SLICE
-        dcn_mesh_shape = (dcn_axis, 1, 1, 1)
         fsdp_axis = num_devices // expert_axis // dcn_axis
+        dcn_mesh_shape = (dcn_axis, 1, 1, 1)
+        ici_mesh_shape = (1, fsdp_axis, expert_axis, 1)
         spmd_mesh = xs.HybridMesh(ici_mesh_shape=ici_mesh_shape, dcn_mesh_shape=dcn_mesh_shape, axis_names=('dcn', 'fsdp', 'expert', 'tensor'))
         xs.set_global_mesh(spmd_mesh)
 
